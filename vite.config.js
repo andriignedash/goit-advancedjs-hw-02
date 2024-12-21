097a62e6dd8f6c416eb4424c583702fbe679dff3
Andrii Gnedash <andrii.gnedash@ringcentral.com>
2024-12-21 16:47:30 +0100

    fix

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig(({ command }) => {
       [command === 'serve' ? 'global' : '_global']: {},
     },
     root: 'src',
+    base: '/goit-advancedjs-hw-02/',
     build: {
       sourcemap: true,
       rollupOptions: {
